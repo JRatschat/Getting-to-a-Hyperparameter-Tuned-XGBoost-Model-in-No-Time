99e84f49c839d4d1e28c99a14cca4caa3e23c147
Jonathan Ratschat <j.ratschat@protonmail.com>
2020-05-19 18:36:43 +0200

    Update code

--- main.R
+++ main.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(data.table)
 library(xgboost)
 library(caret)
+library(ggplot2)
+library(cowplot)
 
 # Load data
 train <- read.csv("data/train.csv")
@@ -13,29 +15,53 @@ test <- read.csv("data/test.csv")
 str(train)
 str(test)
 
-# Delete "unneccessary" columns; the name variable could be used to extract the title
+# Save Survived and passenger ID for later
+Survived <- train$Survived
+PassengerId <- test$PassengerId
+
+# Create new column that indicates if it the train or the test set
+train$Data <- "train"
+test$Data <- "test"
+
+# Delete Survived in train set; we saved it earlier
 train <- train %>%
-  dplyr::select(-PassengerId, -Name, -Ticket, -Cabin)
-test <- test %>%
+  dplyr::select(-Survived)
+
+# Bind rows to create a single data set
+total <- rbind(train, test)
+
+# Delete "unneccessary" columns; the name variable could be used to extract the title
+total <- total %>%
   dplyr::select(-PassengerId, -Name, -Ticket, -Cabin)
 
 # Transform all character variables into factor variables
-train <- train %>% 
+total <- total %>% 
   dplyr::mutate(Sex = as.factor(Sex),
-         Embarked = as.factor(Embarked))
-test <- test %>% 
-  dplyr::mutate(Sex = as.factor(Sex),
-         Embarked = as.factor(Embarked))
+         Embarked = as.factor(Embarked),
+         Data = as.factor(Data))
 
 # Transform integer variable Pclass to factor
-train <- train %>% 
-  dplyr::mutate(Pclass = as.factor(Pclass))
-test <- test %>% 
+total <- total %>% 
   dplyr::mutate(Pclass = as.factor(Pclass))
 
-## Create new family size variable
-train$Family <- train$SibSp + train$Parch
-test$Family <- test$SibSp + test$Parch
+# Create new family size variable
+total$Family <- total$SibSp + total$Parch
+
+# Create one-hot coded matrix
+total_matrix <-model.matrix(~.-1, data = total)
+
+# Split back into train and test set and delete Data
+train <- as.data.frame(total_matrix) %>%
+  dplyr::filter(Datatrain == 1)
+test <- as.data.frame(total_matrix) %>%
+  dplyr::filter(Datatrain == 0)
+train <- train %>%
+  dplyr::select(-Datatrain)
+test <- test %>%
+  dplyr::select(-Datatrain)
+
+# Put Survived back into train
+train$Survived <- Survived
 
 # Randomly select 80% of the observations without replacement 
 set.seed(20)
@@ -49,14 +75,13 @@ validation <- train[-train_id,]
 options(na.action='na.pass')
 
 # Prepare matrix for XGBoost algorithm
-xsell_training_matrix <-model.matrix(Survived ~ .-1, data = training)
-xsell_validation_matrix <-model.matrix(Survived ~ .-1, data = validation)
-xsell_test_matrix <-model.matrix(~.-1, data = test)
-dtrain <- xgb.DMatrix(data = xsell_training_matrix, label = training$Survived) 
-dvalid <- xgb.DMatrix(data = xsell_validation_matrix, label = validation$Survived)
-dtest <- xgb.DMatrix(data = xsell_test_matrix)
+training_matrix <-model.matrix(Survived ~.-1, data = training)
+validation_matrix <-model.matrix(Survived ~.-1, data = validation)
+test_matrix <-model.matrix(~.-1, data = test)
 
-?xgb.DMatrix
+dtrain <- xgb.DMatrix(data = training_matrix, label = training$Survived) 
+dvalid <- xgb.DMatrix(data = validation_matrix, label = validation$Survived)
+dtest <- xgb.DMatrix(data = test_matrix)
 
 # Base XGBoost model
 set.seed(20)
@@ -83,6 +108,14 @@ confusionMatrix(validation$pred_survived_factor_base,
                 positive = "Survived", 
                 dnn = c("Prediction", "Actual Data"))
 
+# Test
+test$pred_survived_base <- predict(xgb_base, dtest)
+test$Survived <- factor(ifelse(test$pred_survived_base > 0.5, 1, 0))
+datasubmission_base<- cbind(as.data.frame(PassengerId), test$Survived)
+datasubmission_base <- datasubmission_base %>%
+  rename("Survived" = "test$Survived")
+write_csv(datasubmission_base, "data/datasubmission_base.csv")
+
 ## Random search
 
 # Take start time to measure time of random search algorithm
@@ -92,10 +125,9 @@ start.time <- Sys.time()
 lowest_error_list = list()
 parameters_list = list()
 
-
 # Create 100 rows with random hyperparameters
 set.seed(20)
-for (iter in 1:500){
+for (iter in 1:10000){
   param <- list(booster = "gbtree",
                 objective = "binary:logistic",
                 max_depth = sample(3:10, 1),
@@ -146,7 +178,53 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
 
-write_csv(randomsearch, "data/randomsearch.csv")
+#write_csv(randomsearch, "data/randomsearch.csv")
+
+# Load random search output
+randomsearch <- read_csv("data/randomsearch.csv")
+
+# Prepare table
+randomsearch <- as.data.frame(randomsearch) %>%
+  rename(val_acc = `1 - min(mdcv$evaluation_log$val_error)`) %>%
+  arrange(-val_acc)
+
+# Tuned-XGBoost model
+set.seed(20)
+params <- list(booster = "gbtree", 
+               objective = "binary:logistic",
+               max_depth = randomsearch[1,]$max_depth,
+               eta = randomsearch[1,]$eta,
+               subsample = randomsearch[1,]$subsample,
+               colsample_bytree = randomsearch[1,]$colsample_bytree,
+               min_child_weight = randomsearch[1,]$min_child_weight)
+xgb_tuned <- xgb.train(params = params,
+                       data = dtrain,
+                       nrounds =1000,
+                       print_every_n = 10,
+                       eval_metric = "auc",
+                       eval_metric = "error",
+                       early_stopping_rounds = 30,
+                       watchlist = list(train= dtrain, val= dvalid))
+
+# Make prediction on dvalid
+validation$pred_survived_tuned <- predict(xgb_tuned, dvalid)
+validation$pred_survived_factor_tuned <- factor(ifelse(validation$pred_survived_tuned > 0.5, 1, 0), 
+                                               labels=c("Not Survived","Survived"))
+
+# Check accuracy with the confusion matrix
+confusionMatrix(validation$pred_survived_factor_tuned, 
+                factor(validation$Survived ,
+                       labels=c("Not Survived", "Survived")),
+                positive = "Survived", 
+                dnn = c("Prediction", "Actual Data"))
+
+# Test
+test$pred_survived_tuned <- predict(xgb_tuned, dtest)
+test$Survived <- factor(ifelse(test$pred_survived_tuned > 0.5, 1, 0))
+datasubmission_tuned <- cbind(as.data.frame(PassengerId), test$Survived)
+datasubmission_tuned <- datasubmission_tuned %>%
+  rename("Survived" = "test$Survived")
+write_csv(datasubmission_tuned, "data/datasubmission_tuned.csv")
 
 
 
@@ -154,6 +232,34 @@ write_csv(randomsearch, "data/randomsearch.csv")
 
 
 
+# Set theme for plots
+mytheme <- theme(
+  ## plotregion
+  panel.background = element_rect(fill = "white",
+                                  colour = "white"),
+  panel.border = element_blank(),
+  panel.grid.major = element_line(size = 0.5, 
+                                  linetype = 'solid',
+                                  colour = "lightgrey"),
+  panel.grid.minor = element_blank(),
+  panel.spacing = unit(0.25, "lines"),
+  ## axis line
+  axis.line = element_line(colour = "black",
+                           size = 0.5, 
+                           linetype = "solid"),
+  ##background of fill
+  strip.background = element_rect(fill="#f2f2f2") +
+    scale_fill_discrete())
 
-
+# Evaluation
+public_leaderboard <- read_csv("data/titanic-publicleaderboard.csv")
+public_leaderboard <- as.data.frame(public_leaderboard) %>%
+  dplyr::select(Score) %>%
+  arrange(-Score)
+library(gghighlight)
+ggplot(public_leaderboard) + geom_histogram(aes(x=Score), binwidth = 0.001) + 
+  scale_x_continuous(limits=c(0.6,0.85)) +
+  mytheme +
+  gghighlight(Score > 0.7799)
+  
 
